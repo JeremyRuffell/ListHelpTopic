a4556be5d60602363d35decf58b70b93e67064d9
Jeremy Ruffell <jruffell@rcs.co.nz>
2019-04-18 17:16:27 +1200

    1.0.2

---
+++ ListHelpTopic/Program.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListHelpTopic
+{
+    class Program
+    {
+        // List that holds Class's HelpTopic Values for the class's that have a bug.
+        public static List<Item> myList = new List<Item>();
+
+        static void Main(string[] args)
+        {
+
+            LoadConfig();
+
+            // Load DLL.
+            Assembly assembly = Assembly.Load("Aquira_WinControls");
+
+            //Get Types from DLL.
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                Type Interface = type.GetInterfaces().Where(x => x.Name == "IGetHelpTopic").FirstOrDefault();
+                if (Interface != null)
+                {
+                    var check = CheckIfValid(type.Name);
+                    if (check != "Ok")
+                    {
+                        Console.WriteLine($"{SpaceGenerator(type.Name)}{check}");
+                    }
+                    else
+                    {
+                        ConstructorInfo ci = type.GetConstructor(new Type[] { });
+
+                        Object v = ci.Invoke(null);
+
+                        MethodInfo m = type.GetProperty("HelpTopic").GetMethod;
+
+                        object o = m.Invoke(v, null);
+
+                        Console.WriteLine($"{SpaceGenerator(type.Name)}'{o.ToString()}'");
+                    }
+                }
+            }
+
+            // Debugging Purposes.
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey();
+        }
+
+        // Item DataModel
+        public class Item
+        {
+            public string ClassName { get; set; }
+            public string HelpTopicReturnValue { get; set; }
+        }
+
+        // To check if Class is bugged and needs to get return value from 'myList'.
+        public static string CheckIfValid(string ClassName)
+        {
+            Item item = myList.FirstOrDefault(x => x.ClassName == ClassName);
+            if (item == null)
+            {
+                return "Ok";
+            }
+            else
+            {
+                return $"'{item.HelpTopicReturnValue}'  *";
+                //return item != null ? item.HelpTopicReturnValue : string.Empty;
+            }
+        }
+
+        // Generates and inserts them after string to provide a nicely formatted console display.
+        public static string SpaceGenerator(string s)
+        {
+            string str = "                                     ";
+            string newstr = str.Remove(str.Length - s.Length);
+            return $"{s}:{newstr}";
+        }
+
+        public class ConfigDataModel
+        {
+
+            public class Rootobject
+            {
+                public List<Toignore> ToIgnore { get; set; }
+            }
+
+            public class Toignore
+            {
+                public string ClassName { get; set; }
+                public string HelpTopicReturnValue { get; set; }
+            }
+
+        }
+
+        // Load config file.
+        public static void LoadConfig()
+        {
+            ConfigDataModel.Rootobject config = JsonConvert.DeserializeObject<ConfigDataModel.Rootobject>(File.ReadAllText("config.json"));
+            foreach (ConfigDataModel.Toignore x in config.ToIgnore)
+            {
+                myList.Add(new Item { ClassName = x.ClassName, HelpTopicReturnValue = x.HelpTopicReturnValue });
+            }
+        }
+    }
+}
---
+++ ListHelpTopic/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ListHelpTopic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ListHelpTopic")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("56b606f8-e816-47fa-b0e3-0241c3cd7673")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
